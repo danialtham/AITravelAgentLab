15e762a242db524a040096e5c70afd2071ccf9e0
Danial <danialtham@microsoft.com>
2025-02-17 21:22:36 +1100

    unit 4 solution

--- AITravelAgent/Program.cs
+++ AITravelAgent/Program.cs
@@ -25,11 +25,40 @@ OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
     FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
 };
 
+string hbprompt = """
+    <message role="system">Instructions: Before providing the the user with a travel itinerary, ask how many days their trip is.</message>
+    <message role="user">I'm going to Rome. Can you create an itinerary for me?</message>
+    <message role="assistant">Sure, how many days is your trip?</message>
+    <message role="user">{{input}}</message>
+""";
+
+var templateFactory = new HandlebarsPromptTemplateFactory();
+var promptTemplateConfig = new PromptTemplateConfig()
+{
+    Template = hbprompt,
+    TemplateFormat = "handlebars",
+    Name = "CreateItinerary",
+};
+
+var function = kernel.CreateFunctionFromPrompt(promptTemplateConfig, templateFactory);
+var plugin = kernel.CreatePluginFromFunctions("TravelItinerary", [function]);
+kernel.Plugins.Add(plugin);
+
 var history = new ChatHistory();
-AddUserMessage("Can you convert 52000 VND to USD?");
-await GetReply();
+history.AddSystemMessage("Before providing destination recommendations, ask the user if they have a budget for their trip.");
 
+Console.WriteLine("Press enter to exit");
+Console.WriteLine("Assistant: How may I help you?");
+Console.Write("User: ");
 
+string input = Console.ReadLine()!;
+while (input != "")
+{
+    history.AddUserMessage(input);
+    await GetReply();
+    Console.Write("User: ");
+    input = Console.ReadLine()!;
+}
 
 # region helper methods
 void AddUserMessage(string msg)
