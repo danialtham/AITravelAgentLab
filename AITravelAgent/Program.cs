a94e128522b8c2a5063f4a52111f5e9af6c01ef6
Danial <danialtham@microsoft.com>
2025-02-17 20:50:20 +1100

    skeleton

---
+++ AITravelAgent/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+
+string yourDeploymentName = "";
+string yourEndpoint = "";
+string yourApiKey = "";
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+    yourDeploymentName,
+    yourEndpoint,
+    yourApiKey,
+    "gpt-4o");
+var kernel = builder.Build();
+
+// Note: ChatHistory isn't working correctly as of SemanticKernel v 1.4.0
+StringBuilder chatHistory = new();
+
+var prompts = kernel.ImportPluginFromPromptDirectory("Prompts");
+
+
