c96db8dffcfb4d1016be3eebe44d98796ec66734
Danial <danialtham@microsoft.com>
2025-02-17 21:00:47 +1100

    use user secret

---
+++ AITravelAgent/Plugins/ConvertCurrency/CurrencyConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AITravelAgent.Plugins.ConvertCurrency
+{
+    public class CurrencyConverter
+    {
+        public static string ConvertAmount(string amount, string baseCurrencyCode, string targetCurrencyCode)
+        {
+            Currency targetCurrency = currencyDictionary[targetCurrencyCode];
+            Currency baseCurrency = currencyDictionary[baseCurrencyCode];
+
+            if (targetCurrency == null)
+            {
+                return targetCurrencyCode + " was not found";
+            }
+            else if (baseCurrency == null)
+            {
+                return baseCurrencyCode + " was not found";
+            }
+        }
+    }
+}
--- AITravelAgent/Program.cs
+++ AITravelAgent/Program.cs
@@ -3,18 +3,17 @@ using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+using Microsoft.Extensions.Configuration;
 
-string yourDeploymentName = "";
-string yourEndpoint = "";
-string yourApiKey = "";
+var builder = new ConfigurationBuilder()
+    .AddUserSecrets<Program>();
+var configuration = builder.Build();
 
-var builder = Kernel.CreateBuilder();
-builder.Services.AddAzureOpenAIChatCompletion(
-    yourDeploymentName,
-    yourEndpoint,
-    yourApiKey,
-    "gpt-4o");
-var kernel = builder.Build();
+string yourDeploymentName = "gpt-4o";
+string yourEndpoint = configuration["AZURE_OPENAI_ENDPOINT_4O"];
+string yourApiKey = configuration["AZURE_OPENAI_API_KEY_4O"];
+
+var kernel = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(yourDeploymentName, yourEndpoint, yourApiKey).Build();
 
 // Note: ChatHistory isn't working correctly as of SemanticKernel v 1.4.0
 StringBuilder chatHistory = new();
